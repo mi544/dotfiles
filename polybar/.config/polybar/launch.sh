b0eb8366940f9ffb07a0bebd13823d49ca24c4ce
Maksim Verkhoturov <sd32@protonmail.com>
2021-08-21 02:08:48 -0600

    adjusts polybar config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar-main.log
+polybar main 2>&1 | tee -a /tmp/polybar-main.log & disown
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar &
-
-echo "Polybar launched..."
+echo "Polybar bar launched..."
