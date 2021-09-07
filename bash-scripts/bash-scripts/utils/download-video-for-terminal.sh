9f746abafcc32dc5dffac94c600fc42987d524bd
Maksim Verkhoturov <sd32@protonmail.com>
2021-09-07 04:00:32 -0600

    copies command to clipboard instead of executing

--- bash-scripts/bash-scripts/utils/download-video-for-terminal.sh
+++ bash-scripts/bash-scripts/utils/download-video-for-terminal.sh
@@ -4,4 +4,5 @@
 
 url_list=$(youtube-dl -f "[height<=720]" -g "$1")
 direct_url=$(echo $url_list | sed "s/\(.*\) .*/\1/")
-ffmpeg -i "$direct_url" -an $HOME/term/$2.mp4
+echo ffmpeg -i \"$direct_url\" -an \"$HOME/term/$2.mp4\" | xclip -selection clipboard
+echo "Clipboard populated with the ffmpeg download command..."
