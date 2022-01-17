434f8290b39675b54f70b5093b45b62d49764de6
Maksim Verkhoturov <sd32@protonmail.com>
2022-01-17 01:23:53 -0700

    adds hypnogram script

---
+++ bash-scripts/bash-scripts/utils/generate-hypnogram.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+
+query="$1"
+query_kebab=$(echo "$query" | sed 's# #-#g')
+
+output_dir="$HOME/Pictures/hypnogram"
+
+if [ ${#query} -eq 0 ]; then
+  echo 'No query received, exiting'
+  exit 0
+fi
+
+if [ ${#query} -ge 75 ]; then
+  echo "Please ensure the query is less than 75 characters long; right now ${#query}"
+  exit 0
+fi
+
+mkdir -p "$output_dir"
+
+echo 'Sending request to hypnogram.xyz..'
+
+counter=0
+while : ; do
+    if [ $counter -gt 50 ]; then
+        echo 'Exhausted 50 retries, aborting'
+        exit 0
+    fi
+
+    counter=$(($counter + 1))
+    hypno_response=$(curl 'https://api.hypnogram.xyz/generate' \
+        --fail \
+        --silent \
+        -X POST \
+        -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0' \
+        -H 'Accept: application/json' \
+        -H 'Accept-Language: en-US,en;q=0.5' \
+        -H 'Accept-Encoding: gzip, deflate, br' \
+        -H 'Referer: https://hypnogram.xyz/' \
+        -H 'Content-Type: application/json' \
+        -H 'Origin: https://hypnogram.xyz' \
+        -H 'DNT: 1' \
+        -H 'Connection: keep-alive' \
+        -H 'Sec-Fetch-Dest: empty' \
+        -H 'Sec-Fetch-Mode: cors' \
+        -H 'Sec-Fetch-Site: same-site' \
+        -H 'TE: trailers' \
+        --data-raw '
+            {
+                "algorithm": "Hypnogram/1.1.1",
+                "prompt": "'"$query"'",
+                "high_resolution": false
+            }')
+
+    echo 'Response:'
+    echo "$hypno_response"
+
+    if [ ${#hypno_response} -eq 0 ]; then
+        echo "Error in response, image_id was not captured"
+        exit 0
+    fi
+
+    error_code=$(echo "$hypno_response" | jq '.error_code' | sed 's#"##g')
+
+    if [[ "$error_code" = 'STANDARD_QUEUE_FULL' ||
+          "$error_code" = 'TOO_FAST' ]]; then
+        echo 'Error encountered, waiting 10 seconds...'
+        echo "[$error_code]"
+        sleep 10
+        echo 'Retrying'
+        continue
+    fi
+
+    break
+done
+
+image_id=$(echo "$hypno_response" | jq '.image_id' | sed 's#"##g')
+
+echo 'image_id successfully captured'
+echo "[$image_id]"
+
+echo 'Waiting 10 seconds..'
+sleep 10
+
+while : ; do
+    output_path="$output_dir/$query_kebab.jpg"
+    if [ -f "$output_path" ]; then
+        file_counter=2
+        while : ; do
+            if [ -f "$output_dir/$query_kebab""__$file_counter.jpg" ]; then
+                file_counter=$(($file_counter + 1))
+                continue
+            fi
+            output_path="$output_dir/$query_kebab""__$file_counter.jpg"
+            break
+        done
+    fi
+
+    curl "https://s3.amazonaws.com/hypnogram-images/$image_id.jpg" \
+        --fail \
+        --silent \
+        --output "$output_path"
+
+    if [ $? -ne 0  ]; then
+        echo 'Image not ready yet, waiting 5 seconds..'
+        sleep 5
+        continue
+    fi
+
+    echo 'Image saved successfully'
+    echo "[$output_path]"
+    break
+done
