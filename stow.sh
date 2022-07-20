fb4fa0aa33a7b2ccd9a69e40c35a761d4a47bff0
Maksim Verkhoturov <sd32@protonmail.com>
2022-07-19 18:10:39 -0600

    misc updates, improvements

--- stow.sh
+++ stow.sh
@@ -4,4 +4,4 @@
 find . | grep -v '.git' | grep ".config/" | sed "s|.*\(\.config.*\)|$HOME/\1|g" | xargs rm -rf
 
 # stowing everything from this directory
-ls -I "README.md" -I "stow.sh" | xargs stow -v
+ls -I 'README.md|stow.sh|fcrontab' | xargs stow -v
